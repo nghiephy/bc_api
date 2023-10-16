b73a541a366f7cb85593139b1929b0a3515b012a
Nghiep Nguyen <46142505+nghiephy@users.noreply.github.com>
2023-10-16 21:02:45 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -16,6 +16,10 @@ fetch(url, options)
 
 /* GET users listing. */
 router.get("/", function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   res.send({title: "respond with a resource 1", data: dataWishlist});
 });
 
