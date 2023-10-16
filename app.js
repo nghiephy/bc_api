79721017cfb991f8cd238354acd47f6c9e946481
Nghiep Nguyen <nguyenlapnghiep44@gmail.com>
2023-10-16 20:40:22 +0700

    add cros

--- app.js
+++ app.js
@@ -21,6 +21,25 @@ var load = require("./routes/load");
 var uninstall = require("./routes/uninstall");
 // ========================================================
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type,token');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 var app = express();
 app.use(logger("dev"));
 app.use(express.json());
