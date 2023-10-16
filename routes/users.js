573adfcb2bdb76270911add83a0b988aef912372
Nghiep Nguyen <46142505+nghiephy@users.noreply.github.com>
2023-10-16 08:42:59 +0700

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var fetch = require('node-fetch');
 
-let url = 'https://api.bigcommerce.com/stores/vwrie/v3/wishlists';
+let url = 'https://api.bigcommerce.com/stores/vwrie/v3/wishlists/customer_id=1';
 let dataWishlist = '';
 let options = {
   method: 'GET',
